d8e12c282f32db2ad33ff4d23e0bedd6edd9800e
Donny Murdiyono <donnymurdiyono@gmail.com>
2026-05-08 11:04:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# heyYou!
+==========
+A simple CLI program to display quote of the day for You!
+
+Start your day with the right quote whether it's random, simply funny or inspirational.
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+// Main entry point for heyYou CLI app.
+
+// Third-party modules
+import { Command } from "commander";
+
+// Initialize CLI app
+const app = new Command();
+app
+    .name("heyYou")
+    .description("A CLI app to give user a random quote of the day.")
+    .option("-f, --funny", "Start your day with a funny quote.")
+    .option("-i, --inspire", "Get a random inspiring quote of the day.")
+    .parse(process.argv);
+
+const options = app.opts();
+
+// Main logic to handle user input and provide quotes
+if (!process.argv.slice(2).length) {
+    app.outputHelp();
+}
